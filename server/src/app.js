57eda29976992f26fa54c3ba4c497fbc1237e921
Kasun Kavinda <kkhettiarachchi10@gmail.com>
2025-03-02 22:51:01 +0530

    implement the auth routing

---
+++ server/src/app.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const cookieParser = require('cookie-parser');
+const auth = require('./routes/auth');
+const connectDB = require('./config/db');
+
+require('dotenv').config();
+
+app.use(cookieParser());
+app.use(express.json());
+app.use(cors());
+
+connectDB().then(r => {
+
+});
+
+app.use('/api/v1/auth', auth);
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
