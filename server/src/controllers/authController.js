0517dcedd99e4186954b46ee4a77d7c2c109ff2d
Kasun Kavinda <kkhettiarachchi10@gmail.com>
2025-03-03 21:22:32 +0530

    implement the JWT authentication

--- server/src/controllers/authController.js
+++ server/src/controllers/authController.js
@@ -1,21 +1,32 @@
 const { signUpUser } = require('../services/authService');
-const { SignUpDTO } = require('../dtos/SignUpDTO');
+const SignUpDTO = require('../dtos/SignUpDTO');
 
 async function signUp(req, res) {
     try {
+
         // Convert request body to DTO
         const dto = new SignUpDTO(
             req.body.email,
             req.body.password,
-            req.body.confirmPassword,
-            req.body.fullName,
+            req.body.name,
         );
 
         // Call service function
-        const user = await signUpUser(dto);
+        const result  = await signUpUser(dto);
+
+        res.cookie('refreshToken', result.token.refreshToken, {
+            httpOnly: true,
+            secure: true,
+            sameSite: 'strict',
+            maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
+            path: '/'
+        });
 
         // Send response
-        res.status(201).json({ message: 'User signed up successfully', user });
+        return res.status(201).json({
+            user: result.user,
+            token: result.token,
+        });
 
     } catch (error) {
         res.status(400).json({ error: error.message });
